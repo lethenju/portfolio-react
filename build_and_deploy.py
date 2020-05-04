831620e6fb0963de79db2937fa9aa20a614dec77
Julien LE THENO <julien.letheno@gmail.Com>
2020-05-04 16:45:42 +0200

    '{message}'

---
+++ build_and_deploy.py
@@ -0,0 +1,70 @@
+# Automating build and deploy of site.
+# Because the site is deployed on github pages, its in another repository, 
+# so I want to have a way to copy files from the build folder of my dev repo
+# to the deployed repo.
+
+# I could have a git repository inited directly in build folder but I believe its best to make a difference
+# between production repo and development repo
+
+import os
+import subprocess
+import glob
+import shutil
+
+_path = "C:/Users/TEMP.JULIEN-PC/Documents/"
+_dev_dir="portfolio-react"
+_prod_dir="lethenju.github.io"
+
+print("=== AUTOMATED BUILD SCRIPT ===")
+
+print("== Building Website ==")
+
+os.system("cd "+_path+_dev_dir)
+os.system("npm run build")
+
+print("== Remove old files ==")
+
+# Remove files first
+files = glob.glob(_path+_prod_dir+'/*', recursive=True)
+
+for f in files:
+    try:
+        if os.path.isfile(f):
+            os.remove(f)
+        else:
+            shutil.rmtree(f)
+    except OSError as e:
+        print("Error: %s : %s" % (f, e.strerror))
+
+
+def recursive_copy(src, dest):
+    """
+    Copy each file from src dir to dest dir, including sub-directories.
+    """
+    for item in os.listdir(src):
+        file_path = os.path.join(src, item)
+
+        # if item is a file, copy it
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, dest)
+
+        # else if item is a folder, recurse 
+        elif os.path.isdir(file_path):
+            new_dest = os.path.join(dest, item)
+            os.mkdir(new_dest)
+            recursive_copy(file_path, new_dest)
+
+
+print("== Copy new files ==")
+
+recursive_copy(_path+_dev_dir+"/build", _path+_prod_dir)
+
+
+print("== Deploy ==")
+
+os.system("cd "+_path+_prod_dir)
+
+message = input("Commit message : ");
+os.system("git add *")
+subprocess.run("git commit -m '{message}'", check=True, shell=True) # Todo scrap version number from package.json
+
