6df7e142ba5ed93ebaa7e438cd6e5f23f8b26375
Julien LE THENO <julien.letheno@gmail.Com>
2020-05-07 21:18:05 +0200

    [Up] Gogenda article

--- src/ProjectsMd.js
+++ src/ProjectsMd.js
@@ -2,6 +2,7 @@
 export let projects_articles = 
 {
   winman : {name:"WinMan"},
+  gogenda : {name:"GoGenda"},
   databash : {name:"Databash"},
   gostate : {name:"GoState"},
   testman : {name:"TestMan"},
@@ -30,6 +31,37 @@ Those widgets can be buttons, text, or simpler shapes.
 I gained experience with C development and makefile building while developing this project.
 
 `;
+
+projects_articles.gogenda.markdown = `
+
+
+
+It has been already a couple of time (actually more than a year) that I log most of the things I do on google agenda.
+You may ask, why do I do it ?
+
+I like to have a view on how my days look like, without just relying on my biased point of vue. 
+I really love to see if I worked more a day or another. Dont judge me.
+
+And I was logging everything by hand, in the Google Calendar app on my phone, or more frequently on the website.
+Adding to that, the UX is pretty bad : to create an event with custom color, you had to create one and then modify the color.
+It was also impossible to set up an event from "now", I had to manually redo the 
+
+I wanted to automate a bit this action, as it can be quite overwhelming to have a google calendar tab open at all times..
+Also, google gives some cool apis to play with, and this project was the perfect occasion to try it.
+
+## Description
+
+GoGenda is a Go client of Google Agenda used for logging activities. You can use your custom categories and colors in a config.json file,
+to fit your needs.
+
+![Alt text](res/capture_gogenda.png)
+
+## What I learned
+
+I gained experience with Go development, and OAuth2 security mechanism for the API.
+
+`;
+
 
 projects_articles.databash.markdown = `
 
